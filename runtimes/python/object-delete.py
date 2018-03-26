565eb8388e7c0f99ec46eb95c7f0c906d6655872
Belinda Vennam <beemarie@us.ibm.com>
2018-03-26 10:56:21 -0500

    python actions initial commit

--- runtimes/python/object-delete.py
+++ runtimes/python/object-delete.py
@@ -1,1 +1,33 @@
-#TODO
+import sys
+import json
+import ibm_boto3
+from ibm_botocore.client import Config
+
+def main(args):
+  resultsGetParams = getParamsCOS(args);
+  cos = resultsGetParams['cos']
+  params = resultsGetParams['params']
+  object = cos.delete_object(
+    Bucket=params['Bucket'],
+    Key=params['Key'],
+  )
+  return {'data': str(object)}
+
+
+def getParamsCOS(args):
+  endpoint = args.get("endpoint","https://s3-api.us-geo.objectstorage.softlayer.net")
+  api_key_id = args.get("apikey", args.get("apiKeyId", "")) # or args['__bx_creds']['cloud-object-storage']
+  service_instance_id = args.get("resource_instance_id", args.get("serviceInstanceId", "")) #or args.__bx_creds["cloud-object-storage"][resource_instance_id];
+  ibm_auth_endpoint = args.get("ibmAuthEndpoint", 'https://iam.ng.bluemix.net/oidc/token')
+  cos = ibm_boto3.client('s3',
+    ibm_api_key_id=api_key_id,
+    ibm_service_instance_id=service_instance_id,
+    ibm_auth_endpoint=ibm_auth_endpoint,
+    config=Config(signature_version='oauth'),
+    endpoint_url=endpoint)
+  params = {};
+  params['Bucket'] = args['Bucket'];
+  params['Key'] = args['Key'];
+
+  # delete params.__bx_creds;
+  return {'cos':cos, 'params':params};
