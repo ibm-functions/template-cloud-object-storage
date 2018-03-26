0afa0b0d1e3fedf52606cbe6f0ced22956882f77
Carlos Santana <csantanapr@apache.org>
2018-03-26 12:04:56 -0400

    just deploy ow in travis for now

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -40,21 +40,21 @@ $ANSIBLE_CMD initdb.yml
 $ANSIBLE_CMD wipe.yml
 $ANSIBLE_CMD openwhisk.yml
 
-cd $WHISKDIR
+#d $WHISKDIR
 
-VCAP_SERVICES_FILE="$(readlink -f $WHISKDIR/../tests/credentials.json)"
+#VCAP_SERVICES_FILE="$(readlink -f $WHISKDIR/../tests/credentials.json)"
 
 #update whisk.properties to add tests/credentials.json file to vcap.services.file, which is needed in tests
-WHISKPROPS_FILE="$WHISKDIR/whisk.properties"
-sed -i 's:^[ \t]*vcap.services.file[ \t]*=\([ \t]*.*\)$:vcap.services.file='$VCAP_SERVICES_FILE':'  $WHISKPROPS_FILE
-cat whisk.properties
+#WHISKPROPS_FILE="$WHISKDIR/whisk.properties"
+#sed -i 's:^[ \t]*vcap.services.file[ \t]*=\([ \t]*.*\)$:vcap.services.file='$VCAP_SERVICES_FILE':'  $WHISKPROPS_FILE
+#cat whisk.properties
 
-WSK_CLI=$WHISKDIR/bin/wsk
-AUTH_KEY=$(cat $WHISKDIR/ansible/files/auth.whisk.system)
-EDGE_HOST=$(grep '^edge.host=' $WHISKPROPS_FILE | cut -d'=' -f2)
+#WSK_CLI=$WHISKDIR/bin/wsk
+#AUTH_KEY=$(cat $WHISKDIR/ansible/files/auth.whisk.system)
+#EDGE_HOST=$(grep '^edge.host=' $WHISKPROPS_FILE | cut -d'=' -f2)
 
 # Set Environment
-export OPENWHISK_HOME=$WHISKDIR
+#export OPENWHISK_HOME=$WHISKDIR
 
 # Place this template in correct location to be included in packageDeploy
 # TODO
