1767587e58edf421a082ad2e936276aa8eb63f61
Belinda Vennam <beemarie@us.ibm.com>
2018-03-26 13:09:22 -0500

    code clean up and read from __bx_creds

--- runtimes/python/object-read.py
+++ runtimes/python/object-read.py
@@ -4,7 +4,7 @@ import ibm_boto3
 from ibm_botocore.client import Config
 
 def main(args):
-  resultsGetParams = getParamsCOS(args);
+  resultsGetParams = getParamsCOS(args)
   cos = resultsGetParams['cos']
   params = resultsGetParams['params']
   object = cos.get_object(
@@ -15,20 +15,19 @@ def main(args):
 
 
 def getParamsCOS(args):
-  endpoint = args.get("endpoint","https://s3-api.us-geo.objectstorage.softlayer.net")
-  api_key_id = args.get("apikey", args.get("apiKeyId", "")) # or args['__bx_creds']['cloud-object-storage']
-  service_instance_id = args.get("resource_instance_id", args.get("serviceInstanceId", "")) #or args.__bx_creds["cloud-object-storage"][resource_instance_id];
-  ibm_auth_endpoint = args.get("ibmAuthEndpoint", 'https://iam.ng.bluemix.net/oidc/token')
+  endpoint = args.get('endpoint','https://s3-api.us-geo.objectstorage.softlayer.net')
+  api_key_id = args.get('apikey', args.get('apiKeyId', args.get('__bx_creds', {}).get('cloud-object-storage', {}).get('apikey', '')))
+  service_instance_id = args.get('resource_instance_id', args.get('serviceInstanceId', args.get('__bx_creds', {}).get('cloud-object-storage', {}).get('resource_instance_id', '')))
+  ibm_auth_endpoint = args.get('ibmAuthEndpoint', 'https://iam.ng.bluemix.net/oidc/token')
   cos = ibm_boto3.client('s3',
     ibm_api_key_id=api_key_id,
     ibm_service_instance_id=service_instance_id,
     ibm_auth_endpoint=ibm_auth_endpoint,
     config=Config(signature_version='oauth'),
     endpoint_url=endpoint)
-  params = {};
-  params['Bucket'] = args['Bucket'];
-  params['Key'] = args['Key'];
+  params = {}
+  params['Bucket'] = args['Bucket']
+  params['Key'] = args['Key']
 
-  # delete params.__bx_creds;
-  return {'cos':cos, 'params':params};
-  
+  # delete params.__bx_creds
+  return {'cos':cos, 'params':params}
