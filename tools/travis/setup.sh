84dbb08b5acbe8c91a5e373d903ae544a3640798
Belinda Vennam <beemarie88@gmail.com>
2018-04-12 12:50:00 -0500

    Initial commit of cos template (#13)
    * initial commit of cos template
    
    * update test name
    
    * maven dependency
    
    * readme updates

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-set -e
-
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
-ROOTDIR="$SCRIPTDIR/../.."
 HOMEDIR="$SCRIPTDIR/../../../"
 DEPLOYDIR="$HOMEDIR/openwhisk/catalog/extra-packages/packageDeploy"
 
 # clone utilties repo. in order to run scanCode.py
-cd ${HOMEDIR}
-git clone --depth 1 https://github.com/apache/incubator-openwhisk-utilities.git
+cd $HOMEDIR
+git clone https://github.com/apache/incubator-openwhisk-utilities.git
 
 # shallow clone OpenWhisk repo.
 git clone --depth 1 https://github.com/apache/incubator-openwhisk.git openwhisk
@@ -19,8 +16,14 @@ git clone --depth 1 https://github.com/apache/incubator-openwhisk-package-deploy
 
 cd openwhisk
 
+./gradlew \
+:common:scala:install \
+:core:controller:install \
+:core:invoker:install \
+:tests:install
+
 # use runtimes.json that defines python-jessie & IBM Node.js 8
 rm -f ansible/files/runtimes.json
-cp ${ROOTDIR}/ansible/files/runtimes.json ansible/files/runtimes.json
+cp $HOMEDIR/template-cloud-object-storage/ansible/files/runtimes.json ansible/files/runtimes.json
 
 ./tools/travis/setup.sh
