477689fecfd4975b4b0a6ee9d28ce6b9f80e1025
eweiter <eweiter@us.ibm.com>
2018-05-31 14:05:09 -0400

    refactored travis code and allowing non-ssl for local&Travis in initial-html

--- runtimes/nodejs/actions/initialHtml.js
+++ runtimes/nodejs/actions/initialHtml.js
@@ -25,7 +25,7 @@ async function main(args) {
     CORSRules: [{
       AllowedHeaders: ['*'],
       AllowedMethods: ['PUT', 'GET', 'DELETE'],
-      AllowedOrigins: ['https://openwhisk.ng.bluemix.net'],
+      AllowedOrigins: ['*'],
     }],
   };
   try {
--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
+set -ex
+
+# Build script for Travis-CI.
+
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+ROOTDIR="$SCRIPTDIR/../.."
+WHISKDIR="$ROOTDIR/../openwhisk"
+UTILDIR="$ROOTDIR/../incubator-openwhisk-utilities"
 HOMEDIR="$SCRIPTDIR/../../../"
-DEPLOYDIR="$HOMEDIR/openwhisk/catalog/extra-packages/packageDeploy"
 
 # clone utilties repo. in order to run scanCode.py
-cd $HOMEDIR
-git clone https://github.com/apache/incubator-openwhisk-utilities.git
+cd ${HOMEDIR}
+git clone --depth 1 https://github.com/apache/incubator-openwhisk-utilities.git
 
 # shallow clone OpenWhisk repo.
 git clone --depth 1 https://github.com/apache/incubator-openwhisk.git openwhisk
 
 # shallow clone deploy package repo.
-git clone --depth 1 https://github.com/apache/incubator-openwhisk-package-deploy $DEPLOYDIR
+git clone --depth 1 https://github.com/apache/incubator-openwhisk-package-deploy
+
+# shallow clone deploy package repo.
+git clone --depth 1 https://github.com/ibm-functions/package-cloud-object-storage
 
 cd openwhisk
-
-./gradlew install
-
-# use runtimes.json that defines python-jessie & IBM Node.js 8
-rm -f ansible/files/runtimes.json
-cp $HOMEDIR/template-cloud-object-storage/ansible/files/runtimes.json ansible/files/runtimes.json
-
 ./tools/travis/setup.sh
